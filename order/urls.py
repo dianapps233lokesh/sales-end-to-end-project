0d31628007d5b06c594f26854303703384be093d
lokesh <lokesh.saini@dianapps.com>
2025-07-17 10:55:52 +0530

    Update on order implemented

--- order/urls.py
+++ order/urls.py
@@ -2,5 +2,6 @@ from django.urls import path,include
 from .views import OrderAPI
 
 urlpatterns = [
-    path('order/',OrderAPI.as_view(),name="order-crud")
+    path('order/',OrderAPI.as_view(),name="order-crud"),
+    path('order/<int:pk>/',OrderAPI.as_view(),name="order-update"),
 ]
--- order/views.py
+++ order/views.py
@@ -4,17 +4,17 @@ from rest_framework.response import Response
 from .serializers import OrderSerializer
 from rest_framework import status
 from rest_framework import permissions
-import random
+from .models import Order
+from utils.logger import logging
 
 class OrderAPI(APIView):
     permission_classes=[permissions.IsAuthenticated]
     def post(self,request):
         try:
-            # order_id=random.randint(1000000,99999999)
-            # request.data['order_id']=order_id
+            logging.info(f"current user is {request.user}")
             serializer=OrderSerializer(data=request.data)
             if serializer.is_valid():
-                serializer.save()
+                serializer.save(user=request.user)
                 return Response({
                     'message':"Order created successfully",
                     "data":serializer.data
@@ -31,3 +31,73 @@ class OrderAPI(APIView):
                 "data":str(e)
             },
             status=status.HTTP_400_BAD_REQUEST)
+        
+    def put(self,request,pk):
+        try:
+            order=Order.objects.get(pk=pk)
+        except Exception as e:
+            return Response({
+                'message':"Order not found",
+                "data":str(e)
+            },
+            status=status.HTTP_404_NOT_FOUND)
+        try:
+            serializer=OrderSerializer(instance=order,data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response({
+                    'message':"Order data updated successfully.",
+                    'data':serializer.data
+                },
+                status=status.HTTP_200_OK)
+            return Response({
+                    'message':"data not valid",
+                    'data':serializer.data
+            },
+            status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({
+                'message':"Error occured",
+                "data":str(e)
+            },
+            status=status.HTTP_400_BAD_REQUEST)
+
+
+        
+    def patch(self,request,pk):
+        try:
+            order=Order.objects.get(pk=pk)
+        except Exception as e:
+             return Response({
+                'message':"Order not found",
+                "data":str(e)
+            },
+            status=status.HTTP_404_NOT_FOUND)
+        
+        try:
+            serializer=OrderSerializer(instance=order,data=request.data,partial=True)
+            if serializer.is_valid():
+                serializer.save(user=request.user)
+                return Response({
+                    'message':"Order data updated successfully.",
+                    'data':serializer.data
+                },
+                status=status.HTTP_200_OK)
+            return Response({
+                    'message':"data not valid",
+                    'data':serializer.data
+            },
+            status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({
+                'message':"Error occured",
+                "data":str(e)
+            },
+            status=status.HTTP_400_BAD_REQUEST)
+            
+
+    def delete(self,request):
+        pass
+
+    def get(self,request):
+        pass
