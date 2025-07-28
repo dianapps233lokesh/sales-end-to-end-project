8f24c18cb76a402b4ac7e6f68342bad5d01e8a89
lokesh <lokesh.saini@dianapps.com>
2025-07-28 12:58:54 +0530

    google calendar api implementation completed

--- order/serializers.py
+++ order/serializers.py
@@ -1,12 +1,16 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from .models import Order
-from utils.logger import logging
-from rest_framework.exceptions import ValidationError
 
 User=get_user_model()
 
 class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Order
+        fields='__all__'
+        read_only_fields=['order_id','user']
+
+class OrderSerializerv2(serializers.ModelSerializer):
     class Meta:
         model=Order
         fields='__all__'
--- sales/settings.py
+++ sales/settings.py
@@ -167,15 +167,19 @@ REST_FRAMEWORK = {
         'accounts.renderers.CustomRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
     ],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '10/hour',  # Limit unauthenticated users to 10 requests per hour
+        'user': '100/hour',  # Limit authenticated users to 100 requests per hour
+    },
      'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.PageNumberPagination',
         'PAGE_SIZE':10
-#     'DEFAULT_PERMISSION_CLASSES': (
-#             'rest_framework.permissions.IsAuthenticated',
-# )
+
 }
-
-
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
